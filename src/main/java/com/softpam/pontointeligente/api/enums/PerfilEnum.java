646e4e3569e7d01372d5c5455be492f07fe0d198
PAULINHO-PC\paulo <pamarcolino@gmail.com>
2018-08-16 18:06:00 -0300

    Adicionando as entidades JPA

--- src/main/java/com/softpam/pontointeligente/api/enums/PerfilEnum.java
+++ src/main/java/com/softpam/pontointeligente/api/enums/PerfilEnum.java
@@ -1,5 +1,6 @@
 package com.softpam.pontointeligente.api.enums;
 
 public enum PerfilEnum {
-
+	ROLE_ADMIN,
+	ROLE_USUARIO
 }
--- src/main/java/com/softpam/pontointeligente/api/enums/TipoEnum.java
+++ src/main/java/com/softpam/pontointeligente/api/enums/TipoEnum.java
@@ -1,5 +1,10 @@
 package com.softpam.pontointeligente.api.enums;
 
 public enum TipoEnum {
-
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
 }
